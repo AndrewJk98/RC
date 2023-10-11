4a640c8ab90d2e43003702ae06a8043a52eb2e6e
Eliseo Cueva <eliseoandres18@gmail.com>
2023-10-10 23:53:09 -0500

    Commit Test

--- src/main/java/pe/edu/cibertec/RC/RcApplication.java
+++ src/main/java/pe/edu/cibertec/RC/RcApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RcApplication {
-
+	int number = 10;
 	public static void main(String[] args) {
 		SpringApplication.run(RcApplication.class, args);
 	}
