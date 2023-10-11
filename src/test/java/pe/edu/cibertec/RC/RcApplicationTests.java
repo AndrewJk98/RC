5f821c6277f9e172c8891b4246abf18507964524
CausitaLimena <joelmolinaflores757@gmail.com>
2023-10-10 23:30:34 -0500

    Caracol

---
+++ src/test/java/pe/edu/cibertec/RC/RcApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.RC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
