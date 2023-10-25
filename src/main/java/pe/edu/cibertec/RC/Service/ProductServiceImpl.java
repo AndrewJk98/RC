e6c97cff6526521762fad67187c924cf64c4f12c
Aresmaster156 <i202116468@cibertec.edu.pe>
2023-10-25 15:24:09 -0500

    Services1

--- src/main/java/pe/edu/cibertec/RC/Service/ProductServiceImpl.java
+++ src/main/java/pe/edu/cibertec/RC/Service/ProductServiceImpl.java
@@ -1,4 +1,46 @@
 package pe.edu.cibertec.RC.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.RC.Model.Productos;
+import pe.edu.cibertec.RC.Repository.ProductDao;
+
+import java.util.List;
+
+@Service
 public class ProductServiceImpl implements ProductService {
+    @Autowired
+    private ProductDao productDao;
+    @Override
+    public List<Productos> Listado() {
+        return (List<Productos>) productDao.findAll();
+    }
+
+    @Override
+    public Productos findByProductId(int ProductoId) {
+        return productDao.findById(ProductoId).orElse(null);
+    }
+
+    @Override
+    public Productos agregar(Productos productos) {
+        return productDao.save(productos);
+    }
+
+    @Override
+    public Productos update(int ProductoId, Productos productos) {
+        Productos productosDB = productDao.findById(ProductoId).get();
+        productosDB.setNombreProducto(productos.getNombreProducto());
+        productosDB.setMarcaProducto(productos.getMarcaProducto());
+        productosDB.setDetalleProducto(productos.getDetalleProducto());
+        productosDB.setPrecioProducto(productos.getPrecioProducto());
+        productosDB.setEstadoProducto(productos.getEstadoProducto());
+        productosDB.setImagenProducto(productos.getImagenProducto());
+        return productDao.save(productosDB);
+    }
+
+    @Override
+    public void delete(int ProductoId) {
+        Productos productosDB = productDao.findById(ProductoId).get();
+        productDao.delete(productosDB);
+    }
 }
--- src/main/java/pe/edu/cibertec/RC/Service/UserService.java
+++ src/main/java/pe/edu/cibertec/RC/Service/UserService.java
@@ -1,5 +1,16 @@
 package pe.edu.cibertec.RC.Service;
 
+import pe.edu.cibertec.RC.Model.Usuarios;
+
+import java.util.List;
+
 public interface UserService {
-    
+    public List<Usuarios> findAll();
+    public Usuarios findById(int id );
+    public Usuarios findByEmail(String email);
+    public Usuarios findByPhone(String phone);
+    public Usuarios agregarUsu(Usuarios usuarios);
+    public Usuarios actualizarUsu(int id, Usuarios usuarios);
+    public void delete(int id );
+
 }
